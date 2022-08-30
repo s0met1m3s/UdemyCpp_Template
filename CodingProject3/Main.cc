8babcf66d0f821402ac8b22e30bda595f5c88666
franneck94 <schaffranek.jan@web.de>
2022-08-30 08:42:20 +0200

    up

--- CodingProject3/Main.cc
+++ CodingProject3/Main.cc
@@ -5,10 +5,7 @@
 int main()
 {
     cppmath::Matrix<double> m1(2, 2, -1.0);
-    cppmath::Matrix<double> m2(3, 2, 1.0);
-
-    m1.print_matrix();
-    m2.print_matrix();
+    cppmath::Matrix<double> m2(2, 2, 1.0);
 
     return 0;
 }
