6d192ff85e16babf9dc07c4bc1254276fd8ed93f
franneck94 <schaffranek.jan@web.de>
2022-12-24 10:39:18 +0100

    up

--- 10_STL2/optional.cc
+++ 10_STL2/optional.cc
@@ -1,34 +1,27 @@
 #include <iostream>
 #include <optional>
+#include <string>
 
-std::optional<std::string> func1(const bool b)
-{
-    if (b)
-        return "Hello World";
-    return {};
-}
+using namespace std::string_literals;
 
-std::optional<std::string> func2(const bool b)
+std::optional<std::string> check(const bool flag)
 {
-    return b ? std::optional<std::string>{"Hello World"} : std::nullopt;
-}
-
-void func3(std::optional<std::string> s = std::nullopt)
-{
-    if (s.has_value())
-        std::cout << s.value() << '\n';
-    else
-        std::cout << "None value...\n";
+    if (flag)
+        return "true"s;
+    return {}; // std::nullopt
 }
 
 int main()
 {
-    const auto r1 = func1(false);
-    const auto r2 = func2(false);
+    auto v1 = std::optional<int>{42};
+    std::cout << *v1 << '\n'; // 42
 
-    func3(r1);
-    func3(r2);
-    func3();
+    auto v2 = std::optional<std::string>{"text"s};
+    std::cout << v2.value() << '\n'; // text
+    auto v3 = std::optional<std::string>{};
+    std::cout << v3.value_or("default"s) << '\n'; // default
+
+    std::cout << std::boolalpha << v3.has_value() << '\n';
 
     return 0;
 }
